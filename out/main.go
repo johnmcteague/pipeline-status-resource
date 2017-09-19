9707a5623a37c5431a0eaf48867250eeec511f6b
Caleb Washburn <cwashburn@pivotal.io>
2017-09-19 09:05:47 -0700

    removing vendor directory and adding ci build

--- out/main.go
+++ out/main.go
@@ -11,6 +11,8 @@ import (
 	"github.com/pivotalservices/pipeline-status-resource/models"
 )
 
+var VERSION = "local-build"
+
 func main() {
 	if len(os.Args) < 2 {
 		println("usage: " + os.Args[0] + " <source>")
